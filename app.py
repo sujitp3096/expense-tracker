b5400a93c6bc1ac9a9d50301379a79e4e3939d09
Sujit Pawar <sujitspawar9696@gmail.com>
2026-04-15 16:11:00 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request, redirect, url_for, session
 import sqlite3
 from datetime import date
 from werkzeug.security import generate_password_hash, check_password_hash
-
 app = Flask(__name__)
 def init_db():
     conn = sqlite3.connect("expenses.db")
